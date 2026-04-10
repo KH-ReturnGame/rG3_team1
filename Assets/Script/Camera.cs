f21a2a66c93e4a85aa6832ea29f850f6f25de0b3
우정 <woojeong1279@gmail.com>
2026-04-10 12:12:20 +0900

    아오습박

---
+++ Assets/Script/Camera.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Camera_controller : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        if (target == null) return;
+
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
