86411116868908c3c24251a83fc678a8f0e27592
Lmotorness <jhw0222@iCloud.com>
2026-05-29 17:53:50 +0900

    없데이트 3

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,19 @@ public class GameManager : MonoBehaviour
 
     public int currentStage = 0;
 
+    //public string[,] grid;
+
+    void Start()
+    {
+        // grid = new string[4,4]
+        // {
+        //     { "a", "b", "c", "d" },
+        //     { "e", "f", "g", "h" },
+        //     { "i", "j", "k", "l" }
+        //     { "m", "n", "o", "p" }
+        // };
+    }
+
     void Awake()
     {
         if (Instance == null)
